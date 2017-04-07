d96dcfc8243bf10ce4b80d35948ba4eb5bdf0e8a
Andrea <andrea@crawford.cc>
2017-04-07 13:40:22 -0700

    5.4 trying to get emails to send

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,16 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '6d43dc54a273ce',
+    :password => '5cfa7830798838',
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
