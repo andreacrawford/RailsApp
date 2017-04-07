99b2b7d402895c9b19f6d1eb27c06bf33a9d489a
Andrea <andrea@crawford.cc>
2017-04-07 14:53:48 -0700

    Added default url to production action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'https://bro-reps.herokuapp.com/' }
+
     # SendGrid
   config.action_mailer.delivery_method = :smtp
 
