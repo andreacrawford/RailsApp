db0e43857e7765021c317b1efb43a095aa9c502a
Andrea <andrea@crawford.cc>
2017-04-06 22:56:10 -0700

    5.4 Added devise and signup/login capabilities. DB migration to store users. Styled devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+  resources :users
   resources :products
   get 'static_pages/about'
 
@@ -8,6 +10,8 @@ Rails.application.routes.draw do
   root 'static_pages#landing_page'
 
   post 'static_pages/thank_you'
+
+
 
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
