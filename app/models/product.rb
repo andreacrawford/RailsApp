23f8e7402d20be424a5ee8d66a12765928bcb9f1
Andrea <andrea@crawford.cc>
2017-04-11 21:46:20 -0700

    5.8 added bootstrap styling to comment form, set up capturing first and last name to add to comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,6 +13,11 @@ class Product < ApplicationRecord
   def lowest_rating_comment
     comments.rating_asc.first
   end
+
+  def average_rating
+    comments.average(:rating).to_f
+  end
+  
 end
 
 
