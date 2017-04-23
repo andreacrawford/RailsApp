be34a08f2c1565f07684d1c2e379814b207d5fb7
Andrea <andrea@crawford.cc>
2017-04-23 11:17:00 -0700

    6.2 Unit tests

---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+describe User do
+
+  it "user is not admin" do
+    expect(User.new.admin?).to be false
+  end
+
+end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+describe Product do
+
+  before do
+    @product = Product.create!(name: "shoe")
+    @user = User.create!(email: "test@test.com", password: "123password")
+    @product.comments.create!(rating: 1, user: @user, body: "Awful shoe!")
+    @product.comments.create!(rating: 3, user: @user, body: "So-so")
+    @product.comments.create!(rating: 5, user: @user, body: "LOVE THEM")
+  end
+
+  it "returns the average rating of all comments" do
+    expect(@product.average_rating).to eq 3
+  end
+
+  it "is not valid" do 
+    expect(Product.new(description: "Beautiful pink Nano")).not_to be_valid
+    expect(Product.new(image_url: "http://www.roguefitness.com/media/catalog/product/cache/1/rogue_header_2015/af097278c5db4767b0fe9bb92fe21690/a/r/ar3434_lg.jpg")).not_to be_valid
+  end
+
+end
