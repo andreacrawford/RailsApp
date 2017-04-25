bb1a0fb616943c75f572c6c4c916ae7bfaa65e0f
Andrea <andrea@crawford.cc>
2017-04-24 22:30:20 -0700

    6.3 functional tests

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'devise'
 gem 'cancancan', '~> 1.10'
 gem 'will_paginate', '~> 3.1.0'
+gem 'rails-controller-testing'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+
+describe UsersController, :type => :controller do
+
+  before do
+    @user = User.create!(email: "andrea+50@crawford.cc", password: "123password")
+    @user2 = User.create!(email: "andrea+51@crawford.cc", password: "123password")
+  end
+
+  describe "GET #show" do
+
+    context "User is logged in" do
+      before do
+        sign_in @user
+      end
+
+      it "Loads correct user details" do
+        get :show, params: { id: @user.id }
+        expect(response).to have_http_status(200)
+        expect(assigns(:user)).to eq @user
+      end
+    end
+
+    context "No user is logged in" do
+      it "redirects to login" do
+        get :show, params: { id: @user.id }
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end 
+
+    context "User cannot see other user show page" do
+      before do
+        sign_in @user2
+      end
+
+      it "redirects to root path" do
+        get :show, params: { id: @user.id }
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+end
