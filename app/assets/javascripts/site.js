656341757ccde15adc769c033980480e636217d8
Andrea <andrea@crawford.cc>
2017-05-04 16:59:38 -0700

    6.10 Added AJAX to comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,12 +1,16 @@
+ var refreshRating = function() {
+    $(".rating").raty( { path: "/assets", scoreName: "comment[rating]" });
+    $(".rated").raty( { path: "/assets", 
+      readOnly: true,
+      score: function() {
+        return $(this).attr("data-score");
+      }
+    });
+};
 
-$( document ).on('turbolinks:load', function() {
+
+$( document ).on('turbolinks:load ajaxSuccess', function() {
   $(".alert, .notice").delay(4000).fadeOut(1000);
-  $(".rating").raty( { path: "/assets", scoreName: "comment[rating]" });
-  $(".rated").raty( { path: "/assets", 
-    readOnly: true,
-    score: function() {
-      return $(this).attr("data-score");
-    }
-  });
+  refreshRating();
   $(".img-zoom").elevateZoom();
 });
