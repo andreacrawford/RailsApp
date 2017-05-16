ac16deb7ce6ea42ecbd7b88009024b0c11f73044
Andrea <andrea@crawford.cc>
2017-05-16 11:16:06 -0700

    6.15 display delete icon to admin

--- app/jobs/comment_update_job.rb
+++ app/jobs/comment_update_job.rb
@@ -1,14 +1,14 @@
 class CommentUpdateJob < ApplicationJob
   queue_as :default
 
-  def perform(comment, current_user)
+  def perform(comment)
     # Do something later
-    ProductChannel.broadcast_to(comment.product_id, comment: render_comment(comment, current_user), average_rating: comment.product.average_rating)
+    ProductChannel.broadcast_to(comment.product_id, comment: render_comment(comment), average_rating: comment.product.average_rating)
   end
 
   private
 
-    def render_comment(comment, current_user)
-      CommentsController.render(partial: 'comments/comment', locals: { comment: comment, current_user: current_user })
+    def render_comment(comment)
+      CommentsController.render(partial: 'comments/comment', locals: { comment: comment, current_user: comment.user })
     end
 end
